100634e0c536228169596db65f14486febbe9ae4
Ryan Petrich <rpetrich@gmail.com>
2018-05-01 19:39:21 -0400

    Simplify babel's rewrite of typeof expressions when the identifier they reference is provably in scope

--- host/bundle-compiler.ts
+++ host/bundle-compiler.ts
@@ -1,7 +1,7 @@
 import * as babel from "babel-core";
 import { NodePath } from "babel-traverse";
 import * as types from "babel-types";
-import { BlockStatement, CallExpression, Expression, ForStatement, Identifier, LogicalExpression, Node, UnaryExpression, UpdateExpression, VariableDeclaration, VariableDeclarator } from "babel-types";
+import { BlockStatement, CallExpression, Expression, ForStatement, Identifier, isBinaryExpression, isCallExpression, isConditionalExpression, isIdentifier, isMemberExpression, isStringLiteral, LogicalExpression, Node, UnaryExpression, UpdateExpression, VariableDeclaration, VariableDeclarator } from "babel-types";
 import Concat from "concat-with-sourcemaps";
 import { resolve } from "path";
 import { Chunk, Finaliser, getExportBlock, OutputOptions, Plugin, rollup, SourceDescription } from "rollup";
@@ -172,6 +172,34 @@ function simplifyVoidInitializedVariables() {
 	};
 }
 
+// Rewrite Babel's typeof foo === "undefined" ? "undefined" : babelHelpers.typeof(foo) to babelHelpers.typeof(foo) when foo can be proved to be in scope
+const simplifyTypeofVisitor = {
+	visitor: {
+		UnaryExpression(path: NodePath<UnaryExpression>) {
+			if (path.node.operator === "typeof" && isIdentifier(path.node.argument) && path.scope.getBinding(path.node.argument.name)) {
+				const parent = path.parentPath;
+				if (isBinaryExpression(parent.node) && parent.node.operator === "===") {
+					const opposite = path.getOpposite().node;
+					if (isStringLiteral(opposite) && opposite.value === "undefined") {
+						const grandparent = parent.parentPath;
+						if (isConditionalExpression(grandparent.node) && grandparent.get("test") === parent) {
+							const consequent = grandparent.get("consequent");
+							const alternate = grandparent.get("alternate");
+							if (isStringLiteral(consequent.node) && consequent.node.value === "undefined" && isCallExpression(alternate.node) && alternate.node.arguments.length === 1) {
+								const argument = alternate.node.arguments[0];
+								const callee = alternate.node.callee;
+								if (isIdentifier(argument) && argument.name === path.node.argument.name && isMemberExpression(callee) && !callee.computed && isIdentifier(callee.object) && callee.object.name === "babelHelpers" && isIdentifier(callee.property) && callee.property.name === "typeof") {
+									grandparent.replaceWith(alternate.node);
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+	},
+};
+
 export interface CompiledRoute {
 	route: StaticFileRoute;
 	map?: RawSourceMap;
@@ -309,6 +337,7 @@ export default async function(fileRead: (path: string) => void, input: string, b
 				[require("babel-plugin-transform-es2015-typeof-symbol")],
 				[require("babel-plugin-transform-es2015-unicode-regex")],
 				[require("babel-plugin-transform-exponentiation-operator")],
+				[simplifyTypeofVisitor],
 			],
 		}),
 	];
